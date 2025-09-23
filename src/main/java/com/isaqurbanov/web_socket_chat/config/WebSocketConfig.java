013c7d1ca2ad443d803fbbecfe0505892fff8ea0
Isameddin <isaqurbanov112@gmail.com>
2025-09-24 01:29:38 +0400

    cleared unused imports

--- src/main/java/com/isaqurbanov/web_socket_chat/config/WebSocketConfig.java
+++ src/main/java/com/isaqurbanov/web_socket_chat/config/WebSocketConfig.java
@@ -1,16 +1,10 @@
 package com.isaqurbanov.web_socket_chat.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest; // ✅ servlet one, not reactive
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
-
-import java.security.Principal;
-import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
--- src/main/java/com/isaqurbanov/web_socket_chat/controller/ChatController.java
+++ src/main/java/com/isaqurbanov/web_socket_chat/controller/ChatController.java
@@ -1,12 +1,9 @@
 package com.isaqurbanov.web_socket_chat.controller;
 
 import com.isaqurbanov.web_socket_chat.model.ChatMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
